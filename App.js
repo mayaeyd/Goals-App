b5d3d23be2d0545e42922230bbe44a3e7c3079f1
Maya <104597612+mayaeyd@users.noreply.github.com>
2024-06-14 15:56:04 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
+import {useState} from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible]=useState(false);
+  const [goalsList, setGoalsList]=useState([]);
+
+  function startHandleAddGoal(){
+    setModalIsVisible(true);
+  }
+
+  function endHandleAddGoal(){
+    setModalIsVisible(false);
+  }
+
+  function handleAddGoal(enteredGoalText){
+    setGoalsList(prevGoals => 
+       [
+        ...prevGoals,
+        {text: enteredGoalText, id: Math.random().toString()},
+      ]);
+    endHandleAddGoal();
+  }
+
+  function deleteGoal(id){
+    setGoalsList(prevGoals =>{ 
+    return prevGoals.filter(goal => goal.id !== id);
+  });
+  }
+
+
   return (
+    <>
+    <StatusBar style='light' />
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Button 
+          title='Add New Goal' 
+          color="#a065ec" 
+          onPress={startHandleAddGoal}
+        />
+        <GoalInput 
+          visible={modalIsVisible} 
+          hAddGoal={handleAddGoal} 
+          endModal={endHandleAddGoal}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList 
+            data={goalsList} 
+            alwaysBounceVertical='true'
+            renderItem={itemData => {
+              return <GoalItem 
+                      text={itemData.item.text}
+                      handleItemPress={deleteGoal}
+                      id={itemData.item.id}
+                      />;
+            }} 
+            keyExtractor={(item, index) => { //called to get a key out of every item which will then be attached to the item by FlatList
+              return item.id;
+            }} 
+           />
+           
+        </View>
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container :{
+    paddingTop:50,
+    paddingHorizontal: 16,
+    flex:1,
+    backgroundColor:'#1e085a'
   },
+  goalsContainer: {
+    flex: 5
+  }
 });
+
